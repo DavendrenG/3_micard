bf5728dda2596510ce7e96a9475ddf6f9fd0a134
DavendrenG <88140388+DavendrenG@users.noreply.github.com>
2025-05-03 14:25:10 +0200

    added code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    ),
-  ));
+  runApp(MyApp()); //DG:Ensure MyApp is added as its func () amd commit.
+} //DG: We also close the square brackets here
+
+class MyApp extends StatelessWidget {
+  @override //DG:remove the const functions
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //DG:From where it says remove, we pasted code
+      home: Scaffold(
+        backgroundColor: Colors.pinkAccent,
+        body: Container(),
+      ),
+    );
+  }
 }
